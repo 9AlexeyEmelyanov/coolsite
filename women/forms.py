febde6e9dbd8cd2685eecf357a9ee49f5992b441
9AlexeyEmelyanov <9aleshaeboshu1@gmail.com>
2023-01-09 22:38:20 +0300

    Add new custom validator and update forms

--- women/forms.py
+++ women/forms.py
@@ -1,11 +1,26 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
 
+class AddPostForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = 'Категория не выбрана'
+            
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'category',]
+        widgets ={
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+        
+    #Customer Validation
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок')
-    slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60,'rows': 10}), label='Описание')
-    is_published = forms.BooleanField(label='Опубликован', initial=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории',empty_label='Категория не выбрана')
+        return title
 
--- women/models.py
+++ women/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
@@ -39,3 +40,5 @@ class Women(models.Model):
         verbose_name = 'Известные женщины'
         verbose_name_plural = 'Известные женщины'
         ordering =['-time_create', 'title']
+
+    
